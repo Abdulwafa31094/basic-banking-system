9003c0877bc753d407c9f7a65ca5bb875f1fdd57
Abdulwafa31094 <112803230+Abdulwafa31094@users.noreply.github.com>
2023-02-22 20:49:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let myAccountBalance = parseInt(document.getElementById("myAccountBalance").innerText);
+  console.log(myAccountBalance);
+  function sendMoney() {
+   var enterName = document.getElementById("enterName").value;
+   var enterAmount = parseInt(document.getElementById("enterAmount").value);
+   console.log(enterAmount);
+
+   if (enterAmount > myAccountBalance) {
+     alert("Insufficient Balance.")
+   } else {
+      var findUserBankAccount = enterName + "BankBalance";
+      console.log(findUserBankAccount);
+      var finalAmount = parseInt(document.getElementById(findUserBankAccount).innerHTML) + enterAmount;
+      var myAccountBalance = parseInt(document.getElementById("myAccountBalance").innerText) - enterAmount
+      document.getElementById("myAccountBalance").innerText = myAccountBalance
+      document.getElementById(findUserBankAccount).innerHTML = finalAmount;
+        alert(` Transaction Successful !!  
+       ₹${enterAmount} is sent to ${enterName}@email.com.`)
+
+    // transaction history 
+      var createPTag = document.createElement("li");
+      var textNode = document.createTextNode(`$${enterAmount} is sent to recepient with Email-id ${enterName}@email.com on ${Date()}.`);
+      createPTag.appendChild(textNode);
+      var element = document.getElementById("transaction-history-body");
+      element.insertBefore(createPTag, element.firstChild);
+   }
+}
